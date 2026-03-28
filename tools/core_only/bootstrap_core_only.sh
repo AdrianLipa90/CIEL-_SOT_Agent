3961bbd98df0bfa0b1614dde76bbd9da184c6f44
Adrian Lipa <adrianlipa90@gmail.com>
2026-03-28 18:19:56 +0000

    refactor: migrate sapiens_client source to v0.2

--- tools/core_only/bootstrap_core_only.sh
+++ tools/core_only/bootstrap_core_only.sh
@@ -12,6 +12,6 @@ fi
 source "${VENV_DIR}/bin/activate"
 python -m pip install --upgrade pip setuptools wheel
 python -m pip install -e "${ENGINE_DIR}"
-python -m pip install pytest
+python -m pip install pytest PyYAML
 
 echo "[core-only] bootstrap complete"
